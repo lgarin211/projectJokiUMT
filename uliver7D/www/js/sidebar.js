b5059d87d3343cb607e5e23429651051677af6c6
Agustinus <57866141+lgarin211@users.noreply.github.com>
2025-01-28 06:56:58 +0000

    On branch low

--- uliver7D/www/js/sidebar.js
+++
@@ -1,9 +0,0 @@
-function openSidebar() {
-  document.getElementById("mySidebar").style.width = "250px";
-  document.getElementById("overlay").style.display = "block";
-}
-
-function closeSidebar() {
-  document.getElementById("mySidebar").style.width = "0";
-  document.getElementById("overlay").style.display = "none";
-}
